3a7691e993c886f6c5de98148852c56b1bef90dd
Renatissima <setasjoao@gmail.com>
2018-02-26 14:24:46 +0000

    added hex package to publish project

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Identicon.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Identicon",
+      source_url: "https://github.com/JoaoSetas/Identicon"
     ]
   end
 
@@ -18,13 +22,24 @@ defmodule Identicon.MixProject do
     ]
   end
 
+  defp description() do
+    "Identicon is a image with a unique pattern to identify a user"
+  end
+
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.12"}, 
+      {:ex_doc, "~> 0.14"}, 
       {:egd, github: "erlang/egd"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
+
+  defp package() do
+    [
+      maintainers: ["João Setas"],
+      links: %{"GitHub" => "https://github.com/JoaoSetas/Identicon"}
+    ]
 end
